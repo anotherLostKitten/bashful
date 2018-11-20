93a340e3703cbeff0937f137433af6821406b47e
jesings <jsinger@stuy.edu>
2018-11-20 11:03:12 -0500

    done

--- control.c
+++ control.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include "execute.h"
+#include "parse_args.h"
 
 #define ANSI_COLOR_RED     "\x1b[31m"
 #define ANSI_COLOR_GREEN   "\x1b[32m"
@@ -12,62 +12,17 @@
 #define ANSI_COLOR_CYAN    "\x1b[36m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
 
-int parse_args(char* buff);
 
 int control(){
-    char strbuff[256];
-    char pwdbuff[256];
     while(1){
-        getcwd(pwdbuff,255);
+        char strbuff[1024];
+        char pwdbuff[1024];
+        getcwd(pwdbuff,1024);
         printf(ANSI_COLOR_CYAN"%s"ANSI_COLOR_GREEN" shell$ "ANSI_COLOR_RESET,pwdbuff);
         fflush(stdout);
-        fgets(strbuff,256,stdin);
+        fgets(strbuff,1024,stdin);
         strbuff[strcspn(strbuff, "\n")] = 0;//remove trailing newline from fgets
         if(parse_args(strbuff))
             return 0;
     }
-}
-
-
-int parse_args(char* buff){
-    int spaces = 0;
-    for(int i=0;buff[i];i++){
-        if(buff[i]==' ') 
-            spaces++;
-    }
-    char** args = malloc((spaces+1)*sizeof(char*));
-    int fewerspaces = 0;
-    char* ptr = buff;
-    args[0]=ptr;
-    for(int c = 1; c<=spaces-fewerspaces;c++){
-        while(*(ptr++)!=' ') ;
-        *(ptr-1)=0;
-        args[c]=ptr;
-        if(*(ptr)=='"'){
-            ptr++;
-            char indicator = 1;
-            args[c]++;           
-            while(*(ptr++)&&indicator){
-                 switch(*ptr){
-                     case ' ':
-                         fewerspaces++;
-                         break;
-                     case '"':
-                         ptr[0]=0;
-                         if(c<spaces-fewerspaces)
-                             ptr=&ptr[2];
-                         indicator = 0;
-                         break;
-                 }
-            }
-        }
-    }
-    char** temp = malloc((spaces+1-fewerspaces)*sizeof(char*));
-    for(int i = 0;i<=spaces+fewerspaces;i++)
-        temp[i]=args[i];
-    free(args);
-    args=temp;
-    if(execute(spaces+1,args))
-        return -1;
-    return 0;
 }
--- makefile
+++ makefile
@@ -1,10 +1,12 @@
-all: shell.o control.o execute.o
-	gcc -o shell shell.o control.o execute.o
+all: parse_args.o shell.o control.o execute.o
+	gcc -o shell shell.o control.o execute.o parse_args.o
 shell.o: shell.c
 	gcc -c shell.c
 control.o: control.c
 	gcc -c control.c
 execute.o: execute.c
 	gcc -c execute.c
+parse_args.o: parse_args.c
+	gcc -c parse_args.c
 run: all
 	./shell
