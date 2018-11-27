71a1dbf57eb4a95f8296d870abbd8b6c78344e6d
Theodore Peters <tpeters@stuy.edu>
2018-11-26 21:47:23 -0500

    system for parsing lines improved to support eventual piping; now parses quotes before semicolons

--- parse_args.c
+++ parse_args.c
@@ -4,28 +4,42 @@
 #include <unistd.h>
 #include "execute.h"
 
-int parse_args(char* buff){
-  for(char*i;i=strsep(&buff,";");){
-    int spaces = 0;
-    for(int j=0;i[j];j++)
-      if(i[j]==' '||i[j]==';'){
-	spaces++;
-	for(;i[j]==' '&&i[j+1];j++);
-      }
-    char**args = malloc((spaces+1)*sizeof(char*)),**curarg = args;
-    for(char*j=i;j;){
-      for(;*j==' ';j++);
-      if(!*j)
-	break;
-      if(*j=='"'){
-	j++;
-	*curarg++=strsep(&j,"\"");
-      }else
-	*curarg++=strsep(&j," ");
-      //printf("[%s]\n",*(curarg-1));
-    }
-    if(execute(args)==-1)
-      return -1;
-  }
-  return 0;
+int parse_args(char*buff){
+	char**carg=malloc(256),**argv=carg,*cur=buff;
+	while(*buff){
+		if(!*cur){
+			if(*buff)
+				*carg++=buff;
+			*carg=0;
+			break;
+		}else if(*cur=='"'){
+			char*qt=cur+1;
+			strcpy(cur,strsep(&qt,"\""));
+			*carg++=buff;
+			cur=buff=qt;
+		}else if(*cur==';'){
+			*cur=0;
+			if(*buff)
+				*carg++=buff;
+			*carg=0;
+			if(execute(argv)==-1)
+				return -1;
+			else
+				return parse_args(cur+1);
+		}else if(*cur==' '){
+			for(;*cur==' ';*cur++=0);
+			if(*buff)
+				*carg++=buff;
+			buff=cur;
+		}else if(*cur=='|'||*cur=='<'){
+			char t=*cur;
+			*cur=0;
+			if(*buff)
+				*carg++=buff;
+			*carg++=t=='|'?"|":"<";
+			buff=cur+1;
+		}else
+			cur++;
+	}
+	return execute(argv);
 }
