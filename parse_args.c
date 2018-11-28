5f045f347967b331c4719c9f64a9217d83e7fe61
Theodore Peters <tpeters@stuy.edu>
2018-11-27 21:31:39 -0500

    piping!

--- parse_args.c
+++ parse_args.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/wait.h>
 #include "execute.h"
 
 
@@ -35,6 +36,9 @@ int parse_args(char*buff){
 			buff=cur;
 		}else if(*cur=='<'){
 			*cur=0;
+			if(*buff)
+				*carg++=buff;
+			*carg=0;
 			int f = open(*(carg-1),O_CREAT|O_WRONLY|O_APPEND);
 			int tmp = dup(STDOUT_FILENO);
 			dup2(f,STDOUT_FILENO);
@@ -46,8 +50,27 @@ int parse_args(char*buff){
 			*cur=0;
 			if(*buff)
 				*carg++=buff;
-			*carg++="|";
-			buff=cur+1;
+			*carg=0;
+			int p[2];
+			pipe(p);
+			if(fork()){
+				close(p[1]);
+				int tmp = dup(STDIN_FILENO);
+				dup2(p[0],STDIN_FILENO);
+				wait(0);
+				parse_args(cur+1);
+				dup2(tmp,STDIN_FILENO);
+				close(p[0]);
+			}else{
+				close(p[0]);
+				int tmp = dup(STDOUT_FILENO);
+				dup2(p[1],STDOUT_FILENO);
+			    execute(argv);
+				dup2(tmp,STDOUT_FILENO);
+				close(p[1]);
+				exit(0);
+			}
+			return 0;
 		}else
 			cur++;
 	}
