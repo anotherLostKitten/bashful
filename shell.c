19ed76bb49e68427a89d68b08746e6cf081cb629
jesings <34111484+jesings@users.noreply.github.com>
2018-12-04 07:12:04 -0500

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,5 +1,4 @@
 #include "control.h"
-// The main function shouldn't ever return because the exit command sends a SIGTERM signal to the process, and that is the intended way of exiting. This function exists so far only to call the control loop.
 int main(){
     control();
     return 0;
