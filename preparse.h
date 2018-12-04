bead4680322b854231e5a07dd9d520c0eec94b02
jesings <34111484+jesings@users.noreply.github.com>
2018-12-04 07:04:01 -0500

    Update preparse.h

--- preparse.h
+++ preparse.h
@@ -5,8 +5,8 @@ struct doubly_ll;
  * dll is the doubly linked list that corresponds to the command the user has entered so far.
  */
 int preparse(struct doubly_ll* dll);
-/* Adds to the doubly linked list the value of the first entry in the current directory (/ for first argumend or cwd otherwise) whose beginning matches word.
- * dll is the doubly linked list of the current terminal entry, first_word_flag denotes whether direcory used should be / or cwd, and word is the word that would be autocompleted.
+/* Adds to the doubly linked list the value of the first entry in the current directory (/bin for first argument or cwd otherwise) whose beginning matches word.
+ * dll is the doubly linked list of the current terminal entry, first_word_flag denotes whether direcory used should be /bin or cwd, and word is the word that would be autocompleted.
  */
 int autocomplete(struct doubly_ll* dll,char first_word_flag,char* word);
 #endif
